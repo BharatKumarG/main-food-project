2585420dadb779e3b7d1ac1d909c734cc2954aac
Guduru Bharat Kumar <115140844+BharatKumarG@users.noreply.github.com>
2025-07-24 10:07:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+// src/App.tsx
+
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { Menu } from './components/Menu';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { CartProvider } from './context/CartContext';
+import Login from './pages/Login';
 
 function App() {
   return (
     <CartProvider>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Hero />
-        <Menu />
-        <About />
-        <Contact />
-      </div>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Hero />
+                <Menu />
+                <About />
+                <Contact />
+              </>
+            } />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
     </CartProvider>
   );
 }
